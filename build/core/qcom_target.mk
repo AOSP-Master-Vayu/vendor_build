34bc787caa8ef57bbdcd7f52d1dfe2b5b08ed24b
maitreya29 <maitreyapatni30@gmail.com>
2021-10-04 08:52:26 +0530

    Vendor: Edit qcom_target for our needs
    * A lot of the code here is from the now deleted pathmap file

--- build/core/qcom_target.mk
+++ build/core/qcom_target.mk
@@ -8,11 +8,21 @@ $(if $(USE_DEVICE_SPECIFIC_$(1)), \
 $(call project-set-path,qcom-$(2),$(strip $(path)))
 endef
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
-$(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
-$(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
-$(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
+pathmap_PROJ := \
+    qcom-audio:hardware/qcom-caf/sm8150/audio \
+    qcom-media:hardware/qcom-caf/sm8150/media \
+    qcom-wlan:hardware/qcom-caf/wlan \
+    qcom-display:hardware/qcom-caf/sm8150/display \
+    qcom-bt-vendor:hardware/qcom-caf/bt \
+    qcom-data-ipa-cfg-mgr:vendor/qcom/opensource/data-ipa-cfg-mgr \
+    qcom-dataservices:vendor/qcom/opensource/dataservices \
+    qcom-thermal-hardware/qcom-caf/thermal \
+    qcom-vr:hardware/qcom-caf/vr
+
+$(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/sm8150/audio)
+$(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/sm8150/display)
+$(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/sm8150/media)
 
 $(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
 $(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
@@ -23,4 +33,32 @@ $(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
 PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
 
-endif
+#
+# Returns the path to the requested module's include directory,
+# relative to the root of the source tree.  Does not handle external
+# modules.
+#
+# $(1): a list of modules (or other named entities) to find the includes for
+#
+define include-path-for
+$(foreach n,$(1),$(patsubst $(n):%,%,$(filter $(n):%,$(pathmap_INCL))))
+endef
+
+# Enter project path into pathmap
+#
+# $(1): name
+# $(2): path
+#
+define project-set-path
+$(eval pathmap_PROJ += $(1):$(2))
+endef
+
+# Returns the path to the requested module's include directory,
+# relative to the root of the source tree.
+#
+# $(1): a list of modules (or other named entities) to find the projects for
+define project-path-for
+$(foreach n,$(1),$(patsubst $(n):%,%,$(filter $(n):%,$(pathmap_PROJ))))
+endef
+
+include vendor/lineage/build/core/utils.mk
--- config/BoardConfigAOSP.mk
+++ config/BoardConfigAOSP.mk
@@ -1,1 +1,2 @@
 include vendor/lineage/config/BoardConfigQcom.mk
+include vendor/lineage/build/core/qcom_target.mk
