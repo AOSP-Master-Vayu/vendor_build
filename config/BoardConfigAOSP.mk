b54fc3329d9e63e3b461cd0eb78ceff88974fa18
maitreya29 <maitreyapatni30@gmail.com>
2021-10-04 08:54:35 +0530

    Vendor: Build APN and SPN

--- config/BoardConfigAOSP.mk
+++ config/BoardConfigAOSP.mk
@@ -1,2 +1,12 @@
 include vendor/lineage/config/BoardConfigQcom.mk
 include vendor/lineage/build/core/qcom_target.mk
+
+PRODUCT_SOONG_NAMESPACES += vendor/lineage/prebuilt/common
+
+# Sensitive Phone Numbers list
+PRODUCT_PACKAGES += \
+    sensitive_pn.xml
+
+# World APN list
+PRODUCT_PACKAGES += \
+    apns-conf.xml
