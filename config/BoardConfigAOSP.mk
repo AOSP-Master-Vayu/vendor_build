628f75b3d23a54429247917d88460aa7a0d4b569
maitreya29 <maitreyapatni30@gmail.com>
2021-10-04 09:04:29 +0530

    Vendor: Drop SPN

--- config/BoardConfigAOSP.mk
+++ config/BoardConfigAOSP.mk
@@ -3,10 +3,6 @@ include vendor/build/build/core/qcom_target.mk
 
 PRODUCT_SOONG_NAMESPACES += vendor/build/prebuilt/common
 
-# Sensitive Phone Numbers list
-PRODUCT_PACKAGES += \
-    sensitive_pn.xml
-
 # World APN list
 PRODUCT_PACKAGES += \
     apns-conf.xml
